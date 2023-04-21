4abf84f3833cb8843274e3ac4bafe730b32e849a
Mercy Agunbiade <agunbiademercy3@gmail.com>
2023-04-21 15:21:50 -0700

    a C program that prints without printf or puts and return 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	
-       const char text[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
-        int length = sizeof(text) - 1;
-
-        write(STDERR_FILENO, text, length);
- 
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

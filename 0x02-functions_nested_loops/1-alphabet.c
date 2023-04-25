3b374a334ae869b61343c19ed3ae2d9233e67e80
Mercy Agunbiade <agunbiademercy3@gmail.com>
2023-04-25 06:47:38 -0700

    print the alphabets in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,12 @@
  * Return: void
  */
 void print_alphabet(void)
-{
+
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-       		 _putchar(letter);
+		_putchar(letter);
 	}
-	
-       	_putchar('\n');
-}
+
+	_putchar('\n');

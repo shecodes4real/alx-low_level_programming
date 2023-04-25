c879adf5f4272da433bd47d5401cdc97ce23e02d
Mercy Agunbiade <agunbiademercy3@gmail.com>
2023-04-25 04:41:08 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    char c = '_';
+
+    putchar(c);
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+
+    return (0);
+}

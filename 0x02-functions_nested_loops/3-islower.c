a72872e6299f76b772b8715944efd67e55144ff0
Mercy Agunbiade <agunbiademercy3@gmail.com>
2023-04-25 14:16:56 -0700

    a function that checks for lowercase character and return 1 if true

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,14 +7,13 @@
  */
 int _islower(int c)
 {
-	char i;
-	int islower = 0;
+    if (c >= 'a' && c <= 'z')
+    {
+	return 1;
+    }
+    else
+    {
+	return 0;
 
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		if (i == c)
-			islower = 1;
-	}
-
-	return (islower);
+    }
 }
